faa585860c70fab0b618db38cba6b44fb0995003
Vladyslav Zlochevskyi <zlochvla@fel.cvut.cz>
2024-11-29 12:18:14 +0100

    Added util classes with static methods. Training, evaluating, model hyperparameters config, device recognition

---
+++ src/utils.py
@@ -0,0 +1,72 @@
+import torch
+
+class ModelConfig:
+    LEARNING_RATE = 1e-3
+    BATCH_SIZE = 64
+    EPOCHS = 5
+
+    def __init__(self, learning_rate=LEARNING_RATE, batch_size=BATCH_SIZE, epochs=EPOCHS):
+        self.learning_rate = learning_rate
+        self.batch_size = batch_size
+        self.epochs = epochs
+
+class XILUtils:
+    @staticmethod
+    def define_device() -> str:
+        """
+        Determines the appropriate device for PyTorch computations.
+
+        This function checks the availability of CUDA and MPS (Metal Performance Shaders)
+        and returns the most suitable device for computation. If neither CUDA nor MPS
+        is available, it defaults to using the CPU.
+
+        Returns:
+            str: A string representing the device to be used for computations.
+                 Possible values are "cuda", "mps", or "cpu".
+
+        """
+        device: str  = (
+            "cuda"
+            if torch.cuda.is_available()
+            else "mps"
+            if torch.backends.mps.is_available()
+            else "cpu"
+        )
+        return device
+
+    @staticmethod
+    def train_loop(dataloader: torch.utils.data.DataLoader, model: torch.nn.Module, loss_fn, optimizer, model_config: ModelConfig):
+
+        size = len(dataloader.dataset)
+
+        model.train()
+        for batch, (X, y) in enumerate(dataloader):
+            # Compute prediction and loss
+            pred = model(X)
+            loss = loss_fn(pred, y)
+
+            # Backpropagation
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+
+            if batch % 100 == 0:
+                loss, current = loss.item(), batch * model_config.batch_size + len(X)
+                print(f"loss: {loss:>7f} [{current:>5d}/{size:>5d}]")
+
+    @staticmethod
+    def test_loop(dataloader, model, loss_fn):
+        model.eval()
+        size = len(dataloader.dataset)
+        num_batches = len(dataloader)
+        test_loss, correct = 0, 0
+
+        with torch.no_grad():
+            for X, y in dataloader:
+                pred = model(X)
+                test_loss += loss_fn(pred, y).item()
+                correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+
+            test_loss /= num_batches
+            correct /= size
+            print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Abg loss: {test_loss:>8f} \n")
