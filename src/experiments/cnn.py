907227c7741cc3e7d8cc925ec1ac7fc121d6be6b
Vladyslav Zlochevskyi <zlochvla@fel.cvut.cz>
2025-04-15 18:01:56 +0200

    Updated experiments/cnn.py: + shallow cnn with maskeddropout

--- src/experiments/cnn.py
+++ src/experiments/cnn.py
@@ -1,4 +1,5 @@
 from torch.nn import Module, Sequential, Conv2d, ReLU, MaxPool2d, Flatten, Linear, Softmax
+from src.masked_dropout import MaskedDropout, ScaleLessDropout
 
 
 class CNNTwoConv(Module):
@@ -44,3 +45,39 @@ class CNNTwoConv(Module):
         idx -= features_size
         return self.classifier[idx]
 
+
+class CNNTwoConvWithDropout(CNNTwoConv):
+    def __init__(self, num_classes=10, dropout_prob=0.5, device="cpu"):
+        super(CNNTwoConvWithDropout, self).__init__()
+        self.dropout = MaskedDropout(torch_dropout=ScaleLessDropout(p=dropout_prob))
+        self.features = Sequential(
+            # 1st group
+            Conv2d(in_channels=1, out_channels=16, kernel_size=3, stride=1, padding=1, device=device),
+            ReLU(),
+            MaxPool2d(kernel_size=2, stride=2, return_indices=True),
+            # 2nd group
+            Conv2d(in_channels=16, out_channels=32, kernel_size=3, stride=1, padding=1, device=device),
+            ReLU(),
+            MaxPool2d(kernel_size=2, stride=2, return_indices=True)
+        )
+        self.classifier = Sequential(
+            Flatten(),
+            Linear(in_features=32 * 7 * 7, out_features=128, device=device),
+            # maxpooling reduces dimensionality by half, 7 = 28 (image_size) / (2 * 2)
+            ReLU(),
+            Linear(in_features=128, out_features=num_classes, device=device),
+            Softmax(dim=1)
+        )
+
+    def forward(self, x, explanation_mask=None):
+        if explanation_mask is None:
+            raise RuntimeError(f"explanation_mask must be provided for {self.__class__.__name__}")
+
+        x = self.dropout(x, explanation_mask)
+        for layer in self.features:
+            if isinstance(layer, MaxPool2d):
+                x, indices = layer(x)
+            else:
+                x = layer(x)
+        x = self.classifier(x)
+        return x
